3e24bbcc374476a805e2dbb6c457765e75851963
mukarama-farouk <faroukmukarama@gmail.com>
2024-01-08 19:01:42 +0000

    updates

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,9 @@
 export default function cleanSe(set, startString) {
+  if (!(set instanceof Set) || typeof startString !== 'string') {
+    throw new Error('Invalid input: set must be a Set and startString must be a string');
+  }
   return Array.from(set)
-    .filter((value) => value.startsWith(startString))
+    .filter(value => value !== null && value !== undefined && typeof value === 'string' && value.startsWith(startString))
     .map((value) => value.slice(startString.length))
     .join('-');
 }
