e5e128fa5df7ac6cc945073ee9d15e0a27db7d06
mukarama-farouk <faroukmukarama@gmail.com>
2024-01-08 18:21:52 +0000

    updated the requirement fail

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,5 +1,5 @@
 export default function getListStudentIds(studentsArray) {
   if (!Array.isArray(studentsArray)) return [];
 
-  return studentsArray.map(student => student.id);
+  return studentsArray.map((student) => student.id);
 }
--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,3 @@
 export default function getStudentIdsSum(studentsList) {
-  return studentsList.reduce((acc, student) => {
-    return acc + student.id
-  }, 0);
+  return studentsList.reduce((acc, student) => acc + student.id, 0);
 }
